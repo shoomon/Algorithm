f6e2d6cf6a158c11f917276638c65f4f84ce7f61
shoomon <96561442+shoomon@users.noreply.github.com>
2025-03-23 17:13:39 +0900

    [level 2] Title: 택배상자, Time: 95.18 ms, Memory: 122 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -0,0 +1,35 @@
+//25.03.23
+//4:58 5:13
+import java.util.*;
+
+class Solution {
+    public int solution(int[] order) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        
+        stack.push(0);
+        
+        int cur = 1;
+        for(int i : order){
+            // System.out.println(cur+" "+i+" "+stack.peek()+" "+answer);
+            if(i != cur){ //현재랑 다름
+                if(stack.peek() == i){ //스택에 있음
+                    stack.pop();
+                    answer++;
+                }else{ //스택에 없음
+                    while(cur < i) stack.push(cur++); //i 전까지 모두 스택에 넣음
+                    if(cur == i){ //i랑 같음
+                        cur++;
+                        answer++;
+                    }else{
+                        return answer;
+                    }
+                }
+            }else{
+                cur++;
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
