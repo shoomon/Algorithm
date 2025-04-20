0a29b3545d5d61a634d4a25dd3a96e2b786870a2
shoomon <96561442+shoomon@users.noreply.github.com>
2025-04-20 18:17:17 +0900

    [level 3] Title: 양과 늑대, Time: 3.03 ms, Memory: 82.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -0,0 +1,46 @@
+//25.04.20
+//17:00 답지 참조
+//한 번 방문한 자식 노드는 언제든지 다시 방문 가능
+import java.util.*;
+
+class Solution {
+    static List<Integer>[] adj;
+    static int answer;
+
+    public int solution(int[] info, int[][] edges) {
+        answer = 0;
+        adj = new List[info.length];
+
+        for (int i = 0; i < info.length; i++) adj[i] = new ArrayList<>();
+
+        for (int[] edge : edges) {
+            adj[edge[0]].add(edge[1]);
+        }
+
+        List<Integer> next = new ArrayList<>();
+        next.add(0);
+
+        simulation(0, 0, next, info);
+
+        return answer;
+    }
+
+    static void simulation(int sheep, int wolf, List<Integer> available, int[] info) {
+        for (int i = 0; i < available.size(); i++) {
+            int cur = available.get(i);
+
+            int newSheep = sheep + (info[cur] == 0 ? 1 : 0);
+            int newWolf = wolf + (info[cur] == 1 ? 1 : 0);
+
+            if (newWolf >= newSheep) continue;
+
+            answer = Math.max(answer, newSheep);
+
+            List<Integer> next = new ArrayList<>(available);
+            next.remove(i);
+            next.addAll(adj[cur]);
+
+            simulation(newSheep, newWolf, next, info);
+        }
+    }
+}
